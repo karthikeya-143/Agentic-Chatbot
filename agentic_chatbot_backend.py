ae8a81eae56ea7559571b9a5ef1b57ba152dc508
B.Karthikeya <karthikbatta143@gmail.com>
2026-07-23 23:14:57 +0530

    chabot backend

---
+++ agentic_chatbot_backend.py
@@ -0,0 +1,50 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph.message import add_messages
+
+
+load_dotenv()
+
+llm=ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0
+)
+
+
+class ChatState(TypedDict):
+
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+
+def chat_node(state: ChatState):
+    #take user query from state
+    messages = state['messages']
+    # send to llm
+    response = llm.invoke(messages)
+    # response store state
+    return {'messages': [response]}
+
+
+
+checkpoint = MemorySaver()
+
+graph = StateGraph(ChatState)
+
+# add nodes
+graph.add_node('chat_node', chat_node)
+
+#add edges
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+
+chatbot = graph.compile(checkpointer=checkpoint)
+
+
+
+
